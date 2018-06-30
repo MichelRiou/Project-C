7a92127afb27199099757a0d257e1009ab367758
Michel Riou <michel.riou@laposte.net>
2018-06-30 23:21:54 +0200

    Creation database mdldata + entity user

--- model/Manager.php
+++ model/Manager.php
@@ -7,7 +7,9 @@ class Manager
     protected function dbConnect()
     {
         try{
-             $db = new \PDO('mysql:host=localhost;dbname=calestor1;charset=utf8', 'dbuser', '');
+                //mysql-flyinpizzas.alwaysdata.net //155227_root
+             //$db = new \PDO('mysql:host=localhost;dbname=calestor1;charset=utf8', 'dbuser', '');
+             $db = new \PDO('mysql:host=mysql-flyinpizzas.alwaysdata.net;dbname=flyinpizzas_calestor1;charset=utf8', '155227', 'samovar77');
         } catch (Exception $ex) {
                 echo $ex->getMessage();
         }
