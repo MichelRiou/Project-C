4e95ba2746138abbfd1a5f97d9e722ad086625a6
Michel Riou <michel.riou@laposte.net>
2018-08-08 00:11:18 +0200

    Commit 07/08-3 objet dans objet

--- model/DAOManager.php
+++ model/DAOManager.php
@@ -11,7 +11,7 @@ class DAOManager
         try{
                 //mysql-flyinpizzas.alwaysdata.net //155227_root
              //$db = new \PDO('mysql:host=localhost;dbname=calestor1;charset=utf8', 'dbuser', '');
-             $db = new \PDO('mysql:host=mysql-flyinpizzas.alwaysdata.net;dbname=flyinpizzas_calestor1;charset=utf8', '155227', 'samovar77');
+             $db = new \PDO('mysql:host=mysql-flyinpizzas.alwaysdata.net;dbname=flyinpizzas_calestor1;charset=utf8', '155227', 'samovar77',array(\PDO::ATTR_PERSISTENT=> true));
         } catch (Exception $ex) {
                 echo $ex->getMessage();
         }
